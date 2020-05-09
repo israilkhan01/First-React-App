3bdc64d5ab6089f64c4476934a64fd2215ed77b3
israilkhan01 <44443989+israilkhan01@users.noreply.github.com>
2020-05-09 23:32:58 +0530

    playing around with components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{ Component } from 'react';
+// import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person'
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h2>Hello World!!</h2>
-        
-        <h1 className="App-link"> This is the first react App</h1>
-        <Person name="Israil" Age="21"/>
-        <Person name="Aman" Age="21">My hobbies is: Playing pubg</Person>
-      </header>
-    </div> 
-  );
+class App extends Component{
+  state={
+    persons:[
+      { name:"Israil khan", dream:"Google"},
+    ],
+    otherStuff:"pubg lover"
+  }
+  switchNameHandler=()=>{
+    // console.log("Was Clicked!!"); 
+    // DON'T Do this this.state.persons[0].name="Fat Boy";
+    this.setState({
+      persons:[
+        { name:"Aliza", dream:"Microsoft"},
+      ]
+    })
+  }
+  switchChangeHandler=(event)=>{
+       this.setState({
+      persons:[
+        { name:event.target.value, dream:"Microsoft"},
+      ]
+    })
+  }
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h2>Hello World!!</h2>
+          <button onClick={this.switchNameHandler}>Switch Name!</button>
+          <h1 className="App-link"> This is the first react App</h1>
+          <Person name={this.state.persons[0].dream} Age="21" change={this.switchChangeHandler}/>
+          <Person name={this.state.persons[0].name} Age="21" click={this.switchNameHandler}>My hobbies is: Playing pubg</Person>
+        </header>
+      </div> 
+    );
+  }
 }
+
 
 export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,9 +3,10 @@ import React from 'react'
 const person=(props)=>{
 return ( 
       <div>
-          <p>I am a person who is Building this App and I am {props.name} and  {props.Age} year old
+          <p onClick={props.click}>I am a person who is Building this App and I am {props.name} and  {props.Age} year old
           </p>
         <p>{props.class}</p>
+        <input type="text" onChange={props.change}/>
       </div>
     )
 };
